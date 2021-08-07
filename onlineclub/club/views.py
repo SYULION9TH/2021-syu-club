a23b334b627989110459c24ca1ecb22f3ef91e1f
hansel <rogitun@gmail.com>
2021-08-07 23:40:23 +0900

    hansel1

--- onlineclub/club/views.py
+++ onlineclub/club/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import *
+import operator
+from datetime import datetime
 
-# Create your views here.
+def home(request):
+    club = Clubs.objects.all().order_by('?')
+    for i in club:
+        i.deadline = i.D_day(datetime.now())
+    context = {'clubs':club} 
+    if request.GET.get('rank'):
+        rank = sorted(club,key=operator.attrgetter('club_rank')) # 우선순위 정렬
+        context = {'clubs':rank}
+    if request.GET.get('dday'):
+        dday = sorted(club, key=operator.attrgetter('deadline')) #디데이 정렬
+        context = {'clubs':dday}
+    return render(request,'home.html',context)
