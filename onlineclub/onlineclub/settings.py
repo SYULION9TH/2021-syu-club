3ad5a879db61df1cea4427a2460f96abf389af6a
gntodtndls156 <gntodtndls156@gmail.com>
2021-08-09 19:09:29 +0900

    settings.py part of accounts 삭제

--- onlineclub/onlineclub/settings.py
+++ onlineclub/onlineclub/settings.py
@@ -127,6 +127,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# part of accounts
-AUTH_USER_MODEL = 'accounts.AuthUser'
