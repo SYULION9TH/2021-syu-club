2cb494323ab53cd7a9e3db123b6465700ba3094d
gntodtndls156 <gntodtndls156@gmail.com>
2021-08-09 19:16:53 +0900

    views.py 수정

--- onlineclub/accounts/views.py
+++ onlineclub/accounts/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def login(request):
+    # 관리자 로그인
+    ## form.cleaned_date.get
     if (request.method == 'POST'):
         form = AuthenticationForm(request=request, data = request.POST)
 
@@ -18,10 +20,11 @@ def login(request):
             if (user is not None):
                 login(request, user)
                 return redirect('#') # TODO - to redirect HOME.html
-            else :
+            else : 
                 # TODO - if user is None
                 print("Login Fail!")
 
 def logout(request):
+    # 관리자 로그아웃
     logout(request)
-    # TODO - to redirect HOME.html
+    return redirect('#') # TODO - to redirect HOME.html
