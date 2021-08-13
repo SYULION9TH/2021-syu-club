2f0cadf4a647ab6a179714272e8b5fdb8f292ef9
Park-Mi-Ran <gpfk6636@naver.com>
2021-08-13 21:26:17 +0900

    posteditview

--- onlineclub/post/urls.py
+++ onlineclub/post/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from post.views import postView, postEditView
 
 urlpatterns = [
-    
+    path('<int:id>/edit', postEditView.detailpage_update, name="detailupdate"),
+    path('<int:id>/<int:post_id>/post_edit', postEditView.post_update ,name="postupdate"),
 ]
---
+++ onlineclub/post/views/postEditView.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from base.models import *
+
+# def detail(request, id):
+#     club = get_object_or_404(Clubs, club_id=id)
+#     return render(request, 'detail.html', {'club':club})
+
+def post_edit(request, id):
+    edit_post = Posts.objects.get(post_id=id)
+    return render(request, 'post/edit.html', {'post':edit_post})
+
+def post_update(request, id):
+    update_post = Posts.objects.get(post_id=id)
+    update_post.post_title = request.POST['post_title']
+    update_post.post_content = request.POST['post_content']
+    update_post.post_img_url = request.POST['post_img_url']
+    update_post.updated_at = timezone.now()
+    update_post.save()
+    return redirect('detail', str(update_post.post_id))
+
+
+def club(request):
+    #동아리 이름과 동아리 한줄 설명 가져오기 위해서
+    club = Clubs.objects.all()
+    return render(request, 'home.html', {'club':club})
+
+
+
+def detailpage_update(request, id):
+    update_detailpage = Clubs.objects.get(club_id=id)
+
+    if request.method =="POST":
+        
+        update_detailpage.club_img_url = request.POST['club_img_url']
+        update_detailpage.club_desc = request.POST.get('desc','club_desc')
+        update_detailpage.recruitment_content = request.POST.get('recruitment','recruitment_content')
+        update_detailpage.save()
+        return redirect('detail' , str(update_detailpage.club_id))
+
+    else:
+        return render(request,'edit.html',{'club':update_detailpage})
+
